afdceff3b60525a932dfe5dd0ed0ff581d386efb
anjusm <52592648+anjusm@users.noreply.github.com>
2024-02-16 18:26:37 +0530

    Update easy_ocr.py

--- easy_ocr.py
+++ easy_ocr.py
@@ -141,5 +141,5 @@ if image is not None:
 else:
     st.write("Upload an Image")
 
-st.caption("**GitHub: ** [@anjusm](https://github.com/anjusm) ❤️ ")
+st.caption("**GitHub:** [@anjusm](https://github.com/anjusm) ❤️ ")
 
